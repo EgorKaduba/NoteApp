d8a49c696ff40f517f3919fc7c4717320ad8e2b4
egork <egor.kaduba@mail.ru>
2024-11-28 00:55:45 +0300

    Создание главного layout, добавление файла с иконкой

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
 import sys
 from PyQt5.QtGui import QIcon
-from PyQt5.QtWidgets import QWidget, QApplication
+from PyQt5.QtWidgets import QWidget, QApplication, QHBoxLayout, QVBoxLayout
 
 
 class MainWindow(QWidget):
     def __init__(self):
         super().__init__()
+        # Задаём базовые настройки окна (название, иконка, мин. размеры)
         self.setWindowTitle('NoteApp')
         self.setWindowIcon(QIcon('icon.ico'))
         self.setMinimumSize(600, 500)
 
+        self.main_layout = QHBoxLayout()  # Главный layout (в нём распологаются все элементы)
+
 
 if __name__ == '__main__':
+    # Запуск приложения
     app = QApplication(sys.argv)
     window = MainWindow()
     window.show()
